02cba946fb5bf243a76c8ea919f6c179969865a6
George Tegos <george.tegos.22@gmail.com>
2023-07-10 13:59:39 +0100

    Create Student Class

---
+++ students.py
@@ -0,0 +1,16 @@
+class Student:
+
+    def __init__(self,name,cohort):
+        self.name = name
+        self.cohort = cohort
+    
+    def talk():
+        return "I can talk"
+    
+    def say_favourite_language(self,favourite_language):
+        return ("I love " +favourite_language)
+    
+person1 = Student("george",'e65')
+
+print(person1.say_favourite_language("Python"))
+
