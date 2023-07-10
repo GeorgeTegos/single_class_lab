2018b75861d71361d58913b2925a2c14b26746a0
George Tegos <george.tegos.22@gmail.com>
2023-07-10 14:09:21 +0100

    Create Team class

---
+++ team.py
@@ -0,0 +1,19 @@
+class Team:
+
+    def __init__(self,name,players,coach):
+        self.name = name
+        self.players = list(players)
+        self.coach = coach
+
+    def add_player(self,name):
+        self.players.append(name)
+    
+    def has_player(self,name):
+        if name in self.players:
+            return True
+        else:
+            return False
+
+player1 = Team("paok",["george",'bill'],"nick")
+
+print(player1.has_player("george"))
